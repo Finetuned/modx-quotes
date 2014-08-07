229d4973633000c54ede1135597c76d6cba8beec
vundicind <vundicind@gmail.com>
2014-08-07 05:01:05 +0300

    Fixed some typos

--- core/components/quotes/lexicon/en/properties.inc.php
+++ core/components/quotes/lexicon/en/properties.inc.php
@@ -5,7 +5,3 @@ $_lang['prop_quotes.quoteTpl_desc'] = 'Chunk template for quotes quote items';
 $_lang['prop_quotes.showNav_desc'] = 'Shows a quick-nav bar at the top of the quotes';
 $_lang['prop_quotes.navOuterTpl_desc'] = 'Chunk template for outer nav-bar wrapper';
 $_lang['prop_quotes.navItemTpl_desc'] = 'Chunk template for each item in the nav-bar';
-
-$_lang['prop_quotes.resId_desc'] = 'ID of resource displaying a Quotes snippet';
-$_lang['prop_quotes.tpl_desc']   = 'Chunk template for highlighter replacement';
-
